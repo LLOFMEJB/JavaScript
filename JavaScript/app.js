f72be66a10225e891e4ea4dfcaf2a9bd4182eae1
HarryPotter <llofmejb@gmail.com>
2021-06-30 18:29:29 +0300

    work

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,32 +1,32 @@
 console.log('hello world');
-console.info("some info");
-console.warn("some warning");
-console.error("error!");
+console.info('some info');
+console.warn('some warning');
+console.error('error!');
 
-console.log("---------");
+console.log('---------');
 
-var myName = "Harry";
+var myName = 'Harry';
 console.log(myName);
 
-myName = "potter";
+myName = 'potter';
 console.log(myName);
 
-var announce = "welcome to our school";
+var announce = 'welcome to our school';
 console.log(announce);
 
-annuonce = "welcome to hogwarts";
+annuonce = 'welcome to hogwarts';
 console.log(announce);
 
-var num = 9*3
+var num = 9 * 3;
 console.log(num);
 
-console.log("-------------------------");
+console.log('-------------------------');
 
 const PI = 3.14;
 console.log(PI);
 
-const SCHOOL = "wizard school of hogwarts";
-const DB_SERVER = "132.168.11.22:5468/mydb";
+const SCHOOL = 'wizard school of hogwarts';
+const DB_SERVER = '132.168.11.22:5468/mydb';
 
 const MYNUM = 5;
 let myNum1 = 5;
@@ -37,5 +37,11 @@ console.log(myNum1);
 myNum1 = 10;
 console.log(myNum1);
 
-MYNUM = 10;
-console.log(myNum);
+// MYNUM = 10;
+console.log(MYNUM);
+
+console.log('----------------------------');
+
+alert("Selam");
+var isim = prompt("Adın nedir?");
+console.log(isim);
