9e244abd1e2268594605d8fd17830748abd99645
HarryPotter <llofmejb@gmail.com>
2021-06-30 18:04:17 +0300

    work

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -19,3 +19,23 @@ console.log(announce);
 
 var num = 9*3
 console.log(num);
+
+console.log("-------------------------");
+
+const PI = 3.14;
+console.log(PI);
+
+const SCHOOL = "wizard school of hogwarts";
+const DB_SERVER = "132.168.11.22:5468/mydb";
+
+const MYNUM = 5;
+let myNum1 = 5;
+
+console.log(MYNUM);
+console.log(myNum1);
+
+myNum1 = 10;
+console.log(myNum1);
+
+MYNUM = 10;
+console.log(myNum);
