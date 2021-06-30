5de3551f72378972a00b9031baf6175f8a62d2fd
HarryPotter <llofmejb@gmail.com>
2021-06-30 17:20:57 +0300

    work

---
+++ JavaScript/app.js
@@ -0,0 +1,21 @@
+console.log('hello world');
+console.info("some info");
+console.warn("some warning");
+console.error("error!");
+
+console.log("---------");
+
+var myName = "Harry";
+console.log(myName);
+
+myName = "potter";
+console.log(myName);
+
+var announce = "welcome to our school";
+console.log(announce);
+
+annuonce = "welcome to hogwarts";
+console.log(announce);
+
+var num = 9*3
+console.log(num);
