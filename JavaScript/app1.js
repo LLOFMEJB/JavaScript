796cc5a815e6d51799c90238c1d72177314c0df6
HarryPotter <llofmejb@gmail.com>
2021-07-01 12:17:53 +0300

    work

---
+++ JavaScript/app1.js
@@ -0,0 +1,47 @@
+console.log('hello world');
+console.info('some info');
+console.warn('some warning');
+console.error('error!');
+
+console.log('---------');
+
+var myName = 'Harry';
+console.log(myName);
+
+myName = 'potter';
+console.log(myName);
+
+var announce = 'welcome to our school';
+console.log(announce);
+
+annuonce = 'welcome to hogwarts';
+console.log(announce);
+
+var num = 9 * 3;
+console.log(num);
+
+console.log('-------------------------');
+
+const PI = 3.14;
+console.log(PI);
+
+const SCHOOL = 'wizard school of hogwarts';
+const DB_SERVER = '132.168.11.22:5468/mydb';
+
+const MYNUM = 5;
+let myNum1 = 5;
+
+console.log(MYNUM);
+console.log(myNum1);
+
+myNum1 = 10;
+console.log(myNum1);
+
+// MYNUM = 10;
+console.log(MYNUM);
+
+console.log('----------------------------');
+
+alert("Selam");
+var isim = prompt("Adın nedir?");
+console.log(isim);
