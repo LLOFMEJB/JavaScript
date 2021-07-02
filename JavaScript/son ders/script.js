349b89f562ceb74b45788a715164bb1f704674a1
HarryPotter <llofmejb@gmail.com>
2021-07-02 22:41:14 +0300

    work

--- JavaScript/son ders/script.js
+++ JavaScript/son ders/script.js
@@ -1,1 +1,13 @@
-// FOR Loops
+// // WHILE Loops
+// let i = 0;
+// let sum = 0;
+
+// while (i < 6) {
+//   sum = sum + i;
+//   console.log(i);
+//   i++;
+// }
+// console.log(`Total is: ${sum}`);
+
+
+//DO WHILE LOOP
