70c769682b724a112bff8b9c1064a7ac88765c98
HarryPotter <llofmejb@gmail.com>
2021-07-05 16:41:51 +0300

    work

--- JavaScript/son ders/script.js
+++ JavaScript/son ders/script.js
@@ -71,9 +71,9 @@
 // }
 // console.log(total);
 
-for (let i = 0; i < 101; i++) {
-  if      (i % 15 === 0)  console.log(`${i} fizz buzz`);
-  else if (i % 3 === 0)   console.log(`${i} fizz`);
-  else if (i % 5 === 0)   console.log(`${i} buzz`);
-  else console.log(i);
-}
+// for (let i = 0; i < 101; i++) {
+//   if      (i % 15 === 0)  console.log(`${i} fizz buzz`);
+//   else if (i % 3 === 0)   console.log(`${i} fizz`);
+//   else if (i % 5 === 0)   console.log(`${i} buzz`);
+//   else console.log(i);
+// }
