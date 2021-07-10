ac0a4826f2575c5ea23a66900fa445e0467462be
HarryPotter <llofmejb@gmail.com>
2021-07-10 21:11:21 +0300

    work

--- JavaScript/son ders/string.js
+++ JavaScript/son ders/string.js
@@ -25,9 +25,24 @@
 // console.log(s1.concat(s2));
 // console.log(s1+` `+s2);
 
-//CHARAT
-var a = "primitive.\nlerin properti veya metodu olmaz."
-console.log(a.charAt(0));
-console.log(a[0]);
-console.log(a);
-console.log("Harry".charAt(3));
+// //CHARAT
+// var a = "primitive.\nlerin properti veya metodu olmaz."
+// console.log(a.charAt(0));
+// console.log(a[0]);
+// console.log(a);
+// console.log("Harry".charAt(3));
+// console.log(a.charAt(a.length-1));
+
+// //İNCLUDES
+// var str = "lorem ıpsum is simply dummy text of the printing and type setting industyr"
+// console.log(str.includes("simply"));  //true
+// console.log(str.includes("Simply"));   //false
+// console.log("harry".includes("rr"));   //true
+
+// //İNDEXOF
+// var str = "lorem ıpsum is simply dummy text of the simply printing and type setting industry"
+// var n = str.indexOf("simply");  //15nci karakter
+// var n = str.indexOf("x");  //30ncu karakter
+// var n = str.indexOf("xx");  //-1 yani yok
+// console.log(n);
+
