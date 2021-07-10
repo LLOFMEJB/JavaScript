73b39e953d821f9ebbdd29fca33fc285bf63bbf6
HarryPotter <llofmejb@gmail.com>
2021-07-10 20:31:49 +0300

    work

--- JavaScript/son ders/string.js
+++ JavaScript/son ders/string.js
@@ -1,16 +1,28 @@
-//LENGHT
-var str1 = "Hello" 
-var str2 = `world` 
-var str3 = 'Hello world' 
-var str4 = new String("A string OBJECT") //object için new şart
+// //LENGHT
+// var str1 = "Hello" 
+// var str2 = `world` 
+// var str3 = 'Hello world' 
+// var str4 = new String("A string OBJECT") //object için new şart
 
-console.log(typeof str1);
-console.log(typeof str2);
-console.log(typeof str3);
-console.log(typeof str4);
-console.log(str4);
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(typeof str3);
+// console.log(typeof str4);
+// console.log(str4);
 
-console.log("hello merhaba".length);
-console.log(str3.length);
-console.log(str4.length);
-console.log(str1.length);
+// console.log("hello merhaba".length);
+// console.log(str3.length);
+// console.log(str4.length);
+// console.log(str1.length);
+
+// //CONCAT
+// var s1 = "Hello";
+// var s2 = "World";
+// var s3 = s1.concat(s2);
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+// console.log(s1.concat(s2));
+// console.log(s1+` `+s2);
+
+//CHARAT
