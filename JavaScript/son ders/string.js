ac9c29a5bcf377b5cf9ae99ca05ce272778c8262
HarryPotter <llofmejb@gmail.com>
2021-07-10 20:22:45 +0300

    work

---
+++ JavaScript/son ders/string.js
@@ -0,0 +1,16 @@
+//LENGHT
+var str1 = "Hello" 
+var str2 = `world` 
+var str3 = 'Hello world' 
+var str4 = new String("A string OBJECT") //object için new şart
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+console.log(typeof str4);
+console.log(str4);
+
+console.log("hello merhaba".length);
+console.log(str3.length);
+console.log(str4.length);
+console.log(str1.length);
