0bc29f67c52886bd7804b5b2b28cbb4acfb213c3
HarryPotter <llofmejb@gmail.com>
2021-07-15 20:54:06 +0300

    work

--- JavaScript/son ders/array.js
+++ JavaScript/son ders/array.js
@@ -1,2 +1,9 @@
-const cars = ["Audi", "Ferrari", "Lamborghini", "Porsche"]
-console.log(typeof cars);
+// const cars = ["Audi", "Ferrari", "Lamborghini", "Porsche"]
+// console.log(typeof cars);
+// console.log(cars);
+
+// const numbers = new Array(2,10)
+// console.log(numbers);
+
+// const numbers2 = new Array(10)
+// console.log(numbers2);
