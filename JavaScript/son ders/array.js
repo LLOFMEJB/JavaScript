047b7ff9310ba44c729ed0a1a836b7ed49949a7a
HarryPotter <llofmejb@gmail.com>
2021-07-16 16:11:57 +0300

    work

--- JavaScript/son ders/array.js
+++ JavaScript/son ders/array.js
@@ -7,3 +7,22 @@
 
 // const numbers2 = new Array(10)
 // console.log(numbers2);
+
+
+// const cars = ["Audi", "Ferrari", "Lamborghini", "Porsche"]
+// var bestcar = cars[1]
+// console.log(cars);
+// console.log(bestcar);
+// console.log("The best car is obviously " + bestcar);
+
+
+// const cars = ["Audi", "Ferrari", "Lamborghini", "Porsche"]
+// cars[0] = "Mercedes"
+// console.log("The cars are  " + cars);
+// console.log(cars.length);
+
+
+// CONCAT METHOD
+const cars = ["Audi", "Ferrari", "Lamborghini"]
+const cars2 = ["BMW", "Porsche"]
+console.log(cars.concat(cars2, "Honda", 2022)); // + ile yaparsan string olur sonuç
