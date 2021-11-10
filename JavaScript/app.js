e905b45ff09b32f8f54b33d45ec4190368b4cf1b
Muhammed Eroğlu <llofmejb@gmail.com>
2021-11-10 23:52:20 +0300

    6

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,55 +1,67 @@
-console.log("LOG")
-console.info("INFO")
-console.warn("WARN")
-console.error("ERROR")
+// console.log("LOG")
+// console.info("INFO")
+// console.warn("WARN")
+// console.error("ERROR")
 
-var country = prompt("Enter your country:")
-console.log("Your country is" , country)
+// var country = prompt("Enter your country:")
+// console.log("Your country is" , country)
 
-var num1 = 1;
+// var num1 = 1;
+
+// // var num10 = 10;
+// // var num11 = 11;
+// // let num20 = 20;
+// // let num21 = 21;
+// console.log(var1, var2);
+// console.log(let1, let2);
+
+//   var var1 = "var1";
+//   var var2 = "var2";
+//   let let1 = "let1";
+//   let let2 = "let2";
 
 // var num10 = 10;
 // var num11 = 11;
 // let num20 = 20;
 // let num21 = 21;
-console.log(var1, var2);
-console.log(let1, let2);
 
-  var var1 = "var1";
-  var var2 = "var2";
-  let let1 = "let1";
-  let let2 = "let2";
+// var num1 = 1;
 
-var num10 = 10;
-var num11 = 11;
-let num20 = 20;
-let num21 = 21;
+// if (num1 == 1) {
+//     var var1 ;
+//     var var2 ;
+//     let let1 ;
+//     let let2 ;
+// }
+// var1 = "var1";
+// var2 = "var2";
+// let1 = "let1";
+// let2 = "let2";
+// console.log(var1, var2);
+// console.log(let1, let2);
 
-var num1 = 1;
+// function tk() {
+//   {
+//     const takım = 5;
+//     console.log(takım);
+//   }
+//   console.log(takım);
+// }
+// tk();
+// console.log(takım);
 
-if (num1 == 1) {
-    var var1 ;
-    var var2 ;
-    let let1 ;
-    let let2 ;
-}
-var1 = "var1";
-var2 = "var2";
-let1 = "let1";
-let2 = "let2";
-console.log(var1, var2);
-console.log(let1, let2);
+// var num=1;
+// let num=2;
+// console.log(num)
 
-function tk() {
-  {
-    const takım = 5;
-    console.log(takım);
+function func1() {
+  var sayı1 = 60;
+  console.log("func1--", sayı1);
+  if (true) {
+    let sayı2 = 34;
+    var sayı3 = 17;
+    console.log(sayı2, sayı3);
   }
-  console.log(takım);
+  console.log(sayı2, sayı3);
 }
-tk();
-console.log(takım);
-
-var num=1;
-let num=2;
-console.log(num)
+func1()
