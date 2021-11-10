3e189173a0c5c064ac4982a639917143e00bf52a
Muhammed Eroğlu <llofmejb@gmail.com>
2021-11-10 22:23:10 +0300

    2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,10 @@
-// console.log("LOG")
-// console.info("INFO")
-// console.warn("WARN")
-// console.error("ERROR")
+console.log("LOG")
+console.info("INFO")
+console.warn("WARN")
+console.error("ERROR")
 
-// var country = prompt("Enter your country:")
-// console.log("Your country is" , country)
-
+var country = prompt("Enter your country:")
+console.log("Your country is" , country)
 
 var num1 = 1;
 
@@ -21,3 +20,36 @@ console.log(let1, let2);
   let let1 = "let1";
   let let2 = "let2";
 
+var num10 = 10;
+var num11 = 11;
+let num20 = 20;
+let num21 = 21;
+
+var num1 = 1;
+
+if (num1 == 1) {
+    var var1 ;
+    var var2 ;
+    let let1 ;
+    let let2 ;
+}
+var1 = "var1";
+var2 = "var2";
+let1 = "let1";
+let2 = "let2";
+console.log(var1, var2);
+console.log(let1, let2);
+
+function tk() {
+  {
+    const takım = 5;
+    console.log(takım);
+  }
+  console.log(takım);
+}
+tk();
+console.log(takım);
+
+var num=1;
+let num=2;
+console.log(num)
