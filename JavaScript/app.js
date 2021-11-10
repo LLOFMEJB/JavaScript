a9cb7762396fccbcb0857a9da4e5164bba0d09da
Muhammed Eroğlu <llofmejb@gmail.com>
2021-11-10 14:21:25 +0300

    1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,17 @@
-console.log("heloo")
+// console.log("LOG")
+// console.info("INFO")
+// console.warn("WARN")
+// console.error("ERROR")
+
+
+// var country = prompt("Enter your country:")
+// console.log("Your country is" , country)
+
+var num1 = 42;
+
+if (num1 == 42) {
+    var num2 = 22;
+    console.log(num1, num2)
+}
+
+console.log(num1, num2)
