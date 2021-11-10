14bbf0922cbe1b82b31eb76ac5a06ed73d8a1ef2
Muhammed Eroğlu <llofmejb@gmail.com>
2021-11-10 15:57:28 +0300

    1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,15 +3,21 @@
 // console.warn("WARN")
 // console.error("ERROR")
 
-
 // var country = prompt("Enter your country:")
 // console.log("Your country is" , country)
 
-var num1 = 42;
 
-if (num1 == 42) {
-    var num2 = 22;
-    console.log(num1, num2)
-}
+var num1 = 1;
 
-console.log(num1, num2)
+// var num10 = 10;
+// var num11 = 11;
+// let num20 = 20;
+// let num21 = 21;
+console.log(var1, var2);
+console.log(let1, let2);
+
+  var var1 = "var1";
+  var var2 = "var2";
+  let let1 = "let1";
+  let let2 = "let2";
+
