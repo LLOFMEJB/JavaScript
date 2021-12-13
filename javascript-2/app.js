da9dcf37dcb6747e635ccc9327f75037f462e269
Muhammed Eroğlu <llofmejb@gmail.com>
2021-12-13 16:08:21 +0300

    3

--- javascript-2/app.js
+++ javascript-2/app.js
@@ -56,3 +56,19 @@
 // // ---5
 
 
+
+
+
+// // TYPEOF
+// console.log(typeof (num))
+
+// var num = 0.1+0.5;
+// console.log(num.toFixed(2));
+// // TOFIXED
+
+
+var a = "Harry"
+console.log("Bana \"Fak\" dedi")
+console.log(`Bana ${a} fak dedi`)
+console.log("H'a'r'r'y Potter")
+console.log('H"a"r"r"y Potter')
