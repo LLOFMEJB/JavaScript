d50e85e0c7296157236796e8c12271f6472c66a4
Muhammed Eroğlu <llofmejb@gmail.com>
2021-12-31 22:03:50 +0300

    9

---
+++ javascript-plus-tekrar/plus_tekrar_app.js
@@ -0,0 +1,30 @@
+// myDiv = document.getElementById("maindiv");
+// console.log(myDiv.innerHTML)
+
+// li1 = document.getElementById("li1");
+// console.log(li1.innerHTML);
+
+// friends = document.getElementsByClassName("mainli");
+// console.log(friends[1].innerHTML);
+
+// bytag = document.getElementsByTagName("li");
+// console.log(bytag);
+
+// bad = document.querySelector("#li3");
+// console.log(bad.innerHTML)
+
+// beautiful = document.querySelectorAll(".girl");
+// console.log(beautiful[1].innerHTML)
+
+// beauty = beautiful[0].innerHTML + " " + beautiful[1].innerHTML;
+// console.log(beauty);
+
+// beautiful[1].innerHTML += " watson";
+
+liler = document.querySelectorAll(".mainli");
+console.log(liler[3].innerHTML)
+
+for (const i of liler) {
+    
+}
+
