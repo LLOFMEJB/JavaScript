e76027b68e26716bf4838383a27a782117a2e05a
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-02 14:35:41 +0300

    3

--- coding-projects/01-gues-the-number/solution/index.js
+++ coding-projects/01-gues-the-number/solution/index.js
@@ -1,36 +1,47 @@
-let number = getRandomInt(0,100);
+let number = Math.floor(Math.random() * 100 + 1);
 console.log(number)
 
-let guess = document.querySelector("#inp").value;
-console.log(guess)
+
+var attemptNum = 0;
+let attempt = document.querySelector("#attempt");
+attempt.innerText = attemptNum
+
+
+document.querySelector("#submitguess").onclick = function() {
+    
+    let guess = document.getElementById("#inp").value;
+    
+    
+    if (number < guess) 
+    {
+        message.classList.add("fail")
+        help.innerHTML = "Try little low!"
+        attemptNum++;
+    } else if (number > guess)
+    {
+        message.classList.add("fail")
+        help.innerHTML = "Try little high!"
+        attemptNum++;
+    } else if (number == guess) 
+    {
+        message.classList.add("success")
+        help.innerHTML = "Absolutely true! Congrats!"
+        attemptNum++;
+    };
+    
+};
 
 let message = document.querySelector("#middle");
 
-let attempt = document.querySelector("#attempt");
+
+
 let help = document.querySelector("#help");
 
-let num = 0;
-attempt.innerHTML = num
-
-function guessFunc(number) {
-    if (number < guess) {
-        num++
-        message.classList.add("fail")
-        help.innerHTML = "Try little low"
-    } else if (number > guess){
-        num++
-        message.classList.add("fail")
-    } else {
-        num++
-        message.classList.add("success")
-        help.innerHTML = "Absolutely true! Congrats!"
-    }
-    
-}
 
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min);
+//   }
