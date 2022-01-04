11e810cd43d89ff06d3cf5065d2ac21ae06201ef
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-04 17:19:15 +0300

    8

--- javascript-plus/clarusbank/signup.js
+++ javascript-plus/clarusbank/signup.js
@@ -1,81 +1,121 @@
-//Form events
-const registrationForm = document.querySelector("#registration_form");
+const registrationForm = document.getElementById("registration_form");
 registrationForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
+//   alert(
+//     document.getElementsByName("first_name")[0].value +
+//       " " +
+//       document.getElementsByName("last_name")[0].value
+//   );
     window.location.href =
-        "checkout.html?firstname=" +
-        document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
+      "checkout.html?firstname=" +
+      document.getElementsByName("first_name")[0].value +
+      "&lastname" +
+      document.getElementsByName("last_name")[0].value;
 });
 
-// //Keyboard events(keydown)
-// let firstNameInput = document.getElementsByName("first_name")[0];
-// firstNameInput.addEventListener("keydown", (e) => {
-//   console.log(e.key);
-//   // Prevent Different character's name(BACKSPACE etc.)
-//   if (e.key.length == 1) {
-//     //add last
+const lastNameInput = document.getElementsByName("last_name")[0];
+const remainCharSpan = document.querySelector("#charCount")
+
+const showRemainChar = () => {
+    remainCharSpan.innerText = lastNameInput.value.length + "/40";
+};
+lastNameInput.addEventListener("keyup", showRemainChar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let deneme = document.querySelector("[name = last_name]");
+// console.log(deneme.classList)
+
+// //Form events
+// const registrationForm = document.querySelector("#registration_form");
+// registrationForm.addEventListener("submit", function (e) {
 //     e.preventDefault();
-//     //add += later
-//     e.target.value += e.key.toLocaleUpperCase();
-//   }
-//   // firstNameInput.value += e.key.toLocaleUpperCase();
+//     window.location.href =
+//         "checkout.html?firstname=" +
+//         document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
 // });
 
-let lastNameInput = document.getElementsByName("last_name")[0];
-//Add span element (id=charCount)
-let remainCharSpan = document.getElementById("charCount");
-//Don't forget add lastname element's maxlength attribute
-const showRemainChar = () => {
-    // if (lastNameInput.value.length > 0) {
-    //remainCharSpan.style.visibility = "visible";
-    remainCharSpan.innerText = lastNameInput.value.length + "/40";
-    //remainCharSpan.textContent = lastNameInput.value.length + "/40";
-    //}
-    // else{
-    //     remainCharSpan.style.visibility = "hidden";
-    //   }
+// // //Keyboard events(keydown)
+// // let firstNameInput = document.getElementsByName("first_name")[0];
+// // firstNameInput.addEventListener("keydown", (e) => {
+// //   console.log(e.key);
+// //   // Prevent Different character's name(BACKSPACE etc.)
+// //   if (e.key.length == 1) {
+// //     //add last
+// //     e.preventDefault();
+// //     //add += later
+// //     e.target.value += e.key.toLocaleUpperCase();
+// //   }
+// //   // firstNameInput.value += e.key.toLocaleUpperCase();
+// // });
 
-};
-lastNameInput.addEventListener("keyup", showRemainChar);
+// let lastNameInput = document.getElementsByName("last_name")[0];
+// //Add span element (id=charCount)
+// let remainCharSpan = document.getElementById("charCount");
+// //Don't forget add lastname element's maxlength attribute
+// const showRemainChar = () => {
+//     // if (lastNameInput.value.length > 0) {
+//     //remainCharSpan.style.visibility = "visible";
+//     remainCharSpan.innerText = lastNameInput.value.length + "/40";
+//     //remainCharSpan.textContent = lastNameInput.value.length + "/40";
+//     //}
+//     // else{
+//     //     remainCharSpan.style.visibility = "hidden";
+//     //   }
 
-//DatePicker onchange function
-let birthDay = document.getElementsByName("birthday")[0];
-birthDay.addEventListener("change", function () {
-    alert(new Date(this.value).toLocaleDateString() + " date is selected!");
-});
+// };
+// lastNameInput.addEventListener("keyup", showRemainChar);
 
-//Selectbox onchange function
-let selectBox = document.getElementById("paths");
-const getSelectedItem = () => {
-    let selectedPathName = selectBox.options[selectBox.selectedIndex].innerText;
+// //DatePicker onchange function
+// let birthDay = document.getElementsByName("birthday")[0];
+// birthDay.addEventListener("change", function () {
+//     alert(new Date(this.value).toLocaleDateString() + " date is selected!");
+// });
 
-    //alternative method
-    // let selectedPathName = paths.options[paths.selectedIndex].innerText;
-    alert(selectedPathName + " path is selected!")
-  }
-  selectBox.onchange = getSelectedItem;
+// //Selectbox onchange function
+// let selectBox = document.getElementById("paths");
+// const getSelectedItem = () => {
+//     let selectedPathName = selectBox.options[selectBox.selectedIndex].innerText;
 
+//     //alternative method
+//     // let selectedPathName = paths.options[paths.selectedIndex].innerText;
+//     alert(selectedPathName + " path is selected!")
+//   }
+//   selectBox.onchange = getSelectedItem;
 
-//E-mail regex match function onkeyup(Add to e-mail input onclick, add span element(id=lblError))
-const validateEmail = () => {
-    let email = document.getElementsByName("email")[0];
-    let errorSpan = document.getElementById("errorSpan");
-    const regexPattern = /\S+\@+\S+\.+\S/;
-    // if (email.value == "" || email.value.length == 0) {
-    //   errorSpan.style.visibility = "hidden";
-    // }
-    // else {
-    // errorSpan.style.visibility = "visible";
-    if (!regexPattern.test(email.value)) {
-        errorSpan.style.color = "red";
-        // Changing Class Attributes(classes added in style in head??)
-        // errorSpan.className = "redColorClass";
-        errorSpan.innerHTML = "Invalid email address!*";
-    }
-    else {
-        errorSpan.style.color = "green";
-        // Changing Class Attributes(classes added in style in head??)
-        // errorSpan.className = "greenColorClass";
-        errorSpan.innerHTML = "E-mail address is valid!";
-    }
-}
+// //E-mail regex match function onkeyup(Add to e-mail input onclick, add span element(id=lblError))
+// const validateEmail = () => {
+//     let email = document.getElementsByName("email")[0];
+//     let errorSpan = document.getElementById("errorSpan");
+//     const regexPattern = /\S+\@+\S+\.+\S/;
+//     // if (email.value == "" || email.value.length == 0) {
+//     //   errorSpan.style.visibility = "hidden";
+//     // }
+//     // else {
+//     // errorSpan.style.visibility = "visible";
+//     if (!regexPattern.test(email.value)) {
+//         errorSpan.style.color = "red";
+//         // Changing Class Attributes(classes added in style in head??)
+//         // errorSpan.className = "redColorClass";
+//         errorSpan.innerHTML = "Invalid email address!*";
+//     }
+//     else {
+//         errorSpan.style.color = "green";
+//         // Changing Class Attributes(classes added in style in head??)
+//         // errorSpan.className = "greenColorClass";
+//         errorSpan.innerHTML = "E-mail address is valid!";
+//     }
+// }
---
+++ javascript-plus/plus_app.js
@@ -0,0 +1,12 @@
+// const part1 = document.querySelector("#part1");
+// part1.scrollIntoView({behavior: "smooth"});
+
+// const part2 = document.querySelector("#part2");
+// part1.scrollIntoView({behavior: "smooth"});
+
+// const part3 = document.querySelector("#part3");
+// part1.scrollIntoView({behavior: "smooth"});
+
+// const part4 = document.querySelector("#part4");
+// part1.scrollIntoView({behavior: "smooth"});
+
