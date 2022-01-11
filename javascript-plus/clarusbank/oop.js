c2a50b380063fcfb49d203d75a46482000b39b48
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-11 12:07:27 +0300

    3

--- javascript-plus/clarusbank/oop.js
+++ javascript-plus/clarusbank/oop.js
@@ -1,37 +1,37 @@
-// let prop1 = "name";
-// let myObj = {};
-// myObj[prop1] = "harry";
-// console.log(myObj);
+let prop1 = "name";
+let myObj = {};
+myObj[prop1] = "harry";
+console.log(myObj);
 
-// const person1 = {
-//     name: "harry",
-//     surname: "potter"
-// };
+const person1 = {
+    name: "harry",
+    surname: "potter"
+};
 
-// // let person2 = person1;
-// let person2 = {...person1};
+// let person2 = person1;
+let person2 = {...person1};
 
-// person2.name = "james"
+person2.name = "james"
 
-// console.log(person1)
-// console.log(person2)
+console.log(person1)
+console.log(person2)
 
-// delete this.name
+delete this.name
 
-// function func1() {
-//     this.name = "matt";
-//     // console.log(this.age);
-// }
-// const func2 = function() {
-//     this.age = 40;
-//     // console.log(this.name);
-// }
+function func1() {
+    this.name = "matt";
+    // console.log(this.age);
+}
+const func2 = function() {
+    this.age = 40;
+    // console.log(this.name);
+}
 
-// console.log(this.age);
-// console.log(this.name);
-// func1();
-// console.log(this.age);
-// console.log(this.name);
-// func2();
-// console.log(this.age);
-// console.log(this.name);
+console.log(this.age);
+console.log(this.name);
+func1();
+console.log(this.age);
+console.log(this.name);
+func2();
+console.log(this.age);
+console.log(this.name);
