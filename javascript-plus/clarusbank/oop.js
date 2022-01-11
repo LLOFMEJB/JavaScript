3970c0646f232de17933a2418ce7d6f55e39c483
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-11 21:25:55 +0300

    2

--- javascript-plus/clarusbank/oop.js
+++ javascript-plus/clarusbank/oop.js
@@ -1,37 +1,56 @@
-let prop1 = "name";
-let myObj = {};
-myObj[prop1] = "harry";
-console.log(myObj);
+// let prop1 = "name";
+// let myObj = {};
+// myObj[prop1] = "harry";
+// console.log(myObj);
 
-const person1 = {
-    name: "harry",
-    surname: "potter"
+// const person1 = {
+//     name: "harry",
+//     surname: "potter"
+// };
+
+// // let person2 = person1;
+// let person2 = {...person1};
+
+// person2.name = "james"
+
+// console.log(person1)
+// console.log(person2)
+
+// delete this.name
+
+// function func1() {
+//     this.name = "matt";
+//     // console.log(this.age);
+// }
+// const func2 = function() {
+//     this.age = 40;
+//     // console.log(this.name);
+// }
+
+// console.log(this.age);
+// console.log(this.name);
+// func1();
+// console.log(this.age);
+// console.log(this.name);
+// func2();
+// console.log(this.age);
+// console.log(this.name);
+
+
+function Person(name, surName) {
+    this.school = "Hogwarts"
+    this.name = name;
+    this.surname = surName;
 };
 
-// let person2 = person1;
-let person2 = {...person1};
-
-person2.name = "james"
-
-console.log(person1)
-console.log(person2)
-
-delete this.name
-
-function func1() {
-    this.name = "matt";
-    // console.log(this.age);
-}
-const func2 = function() {
-    this.age = 40;
-    // console.log(this.name);
+Person.prototype.getFullName = function() {
+    return this.name + " " + this.surname
 }
 
-console.log(this.age);
-console.log(this.name);
-func1();
-console.log(this.age);
-console.log(this.name);
-func2();
-console.log(this.age);
-console.log(this.name);
+let harry = new Person("Harry", "Potter");
+let draco = new Person("Draco", "Malfoy");
+
+console.log(harry)
+console.log(harry.getFullName())
+console.log(draco)
+console.log(draco.getFullName())
