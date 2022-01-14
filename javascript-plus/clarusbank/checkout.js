e33e905a51d9c13ea8f9b88e2e4d05d94a1bfcd9
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-14 16:08:00 +0300

    3

--- javascript-plus/clarusbank/checkout.js
+++ javascript-plus/clarusbank/checkout.js
@@ -1,37 +1,29 @@
-// const queryString = window.location.search;
-// console.log(queryString);
-// const urlParams = new URLSearchParams(queryString);
-// console.log(urlParams);
-// const firstname = urlParams.get("firstname")
-// const lastname = urlParams.get("lastname")
-// console.log(firstname)
-// console.log(lastname)
+function Product(name, price, quantity){
+    console.log("product's this");
+    console.log(this);
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+};
 
-const updateProductQuantity = (input) => {
-    alert(input.value);
-}
+function Display(){
+    console.log("Display's this");
+    console.log(this);
+    this.name = document.getElementById("name");
+    this.price = document.getElementById("price");
+    this.quantity = document.getElementById("quantity");
+};
 
-document.querySelectorAll(".product-quantity input").forEach
-(function(input) {
-    input.addEventListener("change", function() {
-        updateProductQuantity(this);
-    })    
-});
-
-document.querySelectorAll(".product-removal button").forEach
-(function(button) {
-    button.addEventListener("click", function() {
-        removeProduct(this)
-    })
+document.getElementById("submitBtn").addEventListener("submit", 
+function(e){
+    console.log("...event...");
+    console.log(e);
+    console.log("...forms'this...");
+    console.log(this);
+    e.preventDefault()
+    
 })
 
-const removeProduct = (removeButton) => {
-    let productRow = removeButton.closest(".product");
-    productRow.remove();
-}
-
-// let translate = new Date("3 march 2015 20:21:44");
-// console.log(translate.toLocaleDateString());
 
 
 
@@ -90,7 +82,7 @@ const removeProduct = (removeButton) => {
 //     setTimeout(function(){
 //         loading.style.display = "none";
 //         displayObj.addProduct(product);
-//     }, 3000);
+//     }, 2000);
 // }
 // Display.prototype.clearFields = function() {
 //     // to do
@@ -122,6 +114,34 @@ const removeProduct = (removeButton) => {
 
 
 
+// const queryString = window.location.search;
+// console.log(queryString);
+// const urlParams = new URLSearchParams(queryString);
+// console.log(urlParams);
+// const firstname = urlParams.get("firstname");
+// const lastname = urlParams.get("lastname");
+// alert("welcome " + firstname + " " + lastname);
+
+const updateProductQuantity = (input) => {
+    alert(input.value);
+}
+
+document.querySelectorAll(".product-quantity input").forEach(function(input){
+    input.addEventListener("change", function(){
+        updateProductQuantity(input);
+    });
+});
+
+const removeProduct = (button) => {
+    let productRow = button.parentElement.parentElement;
+    productRow.remove();
+}
+
+document.querySelectorAll(".product-removal button").forEach(function(button){
+    button.addEventListener("click", function(){
+        removeProduct(button);
+    });
+});
 
 
 
@@ -147,35 +167,42 @@ const removeProduct = (removeButton) => {
 
 
 
-// // const queryString = window.location.search;
-// // console.log(queryString);
-// // const urlParams = new URLSearchParams(queryString);
-// // console.log(urlParams);
-// // const firstname = urlParams.get("firstname");
-// // const lastname = urlParams.get("lastname");
-// // alert("welcome " + firstname + " " + lastname);
+
+
+
+// const queryString = window.location.search;
+// console.log(queryString);
+// const urlParams = new URLSearchParams(queryString);
+// console.log(urlParams);
+// const firstname = urlParams.get("firstname")
+// const lastname = urlParams.get("lastname")
+// console.log(firstname)
+// console.log(lastname)
 
 // const updateProductQuantity = (input) => {
 //     alert(input.value);
 // }
 
-// document.querySelectorAll(".product-quantity input").forEach(function(input){
-//     input.addEventListener("change", function(){
-//         updateProductQuantity(input);
-//     });
+// document.querySelectorAll(".product-quantity input").forEach
+// (function(input) {
+//     input.addEventListener("change", function() {
+//         updateProductQuantity(this);
+//     })
 // });
 
-// const removeProduct = (button) => {
-//     let productRow = button.parentElement.parentElement;
+// document.querySelectorAll(".product-removal button").forEach
+// (function(button) {
+//     button.addEventListener("click", function() {
+//         removeProduct(this)
+//     })
+// })
+
+// const removeProduct = (removeButton) => {
+//     let productRow = removeButton.closest(".product");
 //     productRow.remove();
 // }
 
-// document.querySelectorAll(".product-removal button").forEach(function(button){
-//     button.addEventListener("click", function(){
-//         removeProduct(button);
-//     });
-// });
+// let translate = new Date("3 march 2015 20:21:44");
+// console.log(translate.toLocaleDateString());
 
-
-
-
+// document.querySelectorAll(".product-quantity input").forEach(function(input){
