55c89104f0744a667a888161bf74f33700cb69c2
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-15 23:57:58 +0300

    5

--- javascript-plus/callback.js
+++ javascript-plus/callback.js
@@ -20,3 +20,34 @@ function movement(array, callback){
 movement([100, 300, -200, 500, -400, 225, 1000, -700], (val) => {
     console.log(val)
 });
+console.log("--------------------------------------------------------------------------------")
+
+
+function firstLetter(string){
+    let stringArr = string.split(" ");
+    console.log(stringArr);
+    let firstLetterStr  = "";
+    for (let i = 0; i < stringArr.length; i++) {
+        firstLetterStr = firstLetterStr.concat(stringArr[i][0]);
+    };
+    console.log(`The string of first letters ===> ${firstLetterStr}`);
+}
+firstLetter("Osman Seher Muhammed Abdullah Nuriye");
+console.log("--------------------------------------------------------------------------------")
+
+function change(string){
+    let stringArr = string.split("");
+    console.log(stringArr);
+    let changed  = "";
+    for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i] == stringArr[i].toUpperCase()) {
+            changed = changed.concat(stringArr[i].toLowerCase())
+        } else {
+            changed = changed.concat(stringArr[i].toUpperCase())
+        };
+    };
+    console.log(`Changed version ===> ${changed}`);
+}
+change("The Quick Brown Fox");
+console.log("--------------------------------------------------------------------------------")
+
