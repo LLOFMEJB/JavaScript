69826bac6d19736697b89365fafb9afffe478831
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-25 16:21:14 +0300

    3

--- coding-challenges/cc-001-fibonacci/app.js
+++ coding-challenges/cc-001-fibonacci/app.js
@@ -1,15 +1,20 @@
-let sayı = prompt("Sayıyı yazınız: ");
-let deger;
+// let sayı = prompt("Sayıyı yazınız: ");
 
-function fibonacci(sayı) {
-    for (let i = 1; i <= n; i++) {
-        deger = deger + i;
+function fibonacci(n) {
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 0; i <= n; i++) {
+        let deger = num1 + num2;
+        num1 = num2;
+        num2 = deger;
     }
     return deger;
 }
 
 
 sayıHtml = document.querySelector("#sayı");
-sayıHtml.innerHTML = sayı;
+sayıHtml.value = sayı;
 degerHtml = document.querySelector("#deger");
 degerHtml.innerHTML = fibonacci(sayı);
+hesapBtn = document.querySelector("#hesapla");
+hesapBtn.
