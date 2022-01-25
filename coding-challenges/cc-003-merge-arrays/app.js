6f2850c155cde8b093bb89df6905d19c56808140
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-25 17:46:47 +0300

    sort array

--- coding-challenges/cc-003-merge-arrays/app.js
+++ coding-challenges/cc-003-merge-arrays/app.js
@@ -1,8 +1,8 @@
-let array1 = [10,3,42,13];
-let array2 = [45,7,15];
-let merged = [];
-
 function mergeArrays(a,b) {
     return merged = (a.concat(b)).sort((a,b) => a-b)
 }
-console.log(mergeArrays(array1, array2))
+
+console.log(mergeArrays([10,3,45], [45,7,15]))
+
+
+// empty control
