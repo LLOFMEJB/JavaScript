1b94435c53325d839e89cd145ddb34c3da1487f1
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-26 22:25:54 +0300

    3

--- coding-challenges/cc-007-stopwatch/app.js
+++ coding-challenges/cc-007-stopwatch/app.js
@@ -1,6 +1,32 @@
-const minute = document.getElementById("minute").innerHTML = "00";
-const second = document.getElementById("second").innerHTML = "00";
-const split =  document.getElementById("split").innerHTML = "00";
+let time = document.getElementById("time");
+let cont = 0;
+let mins = 0;
+let secs = 0;
+let split = 0;
 
+const startStopBtn = document.getElementById("startStop");
+const resetBtn = document.getElementById("reset");
+
+startStopBtn.addEventListener("click", ()=>{cont++});
+
+let num = 0;
+
+const timer = setInterval(function () {
+  if (cont % 2 == 1) {
+    split++;
+  }
+  if (split == 60) {
+    split = 0;
+    secs++
+  }
+  if (secs == 60) {
+    secs = 0;
+    mins++
+  }
+  if (mins = 60) {
+    mins = 0;
+  }
+  time.innerHTML = `${mins}:${secs}:${split}`
+}, 1.666666666666668);
 
 
