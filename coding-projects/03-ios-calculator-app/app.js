f5216f477db035be4c2c9796587c37a24756c86f
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-28 23:07:23 +0300

    2

--- coding-projects/03-ios-calculator-app/app.js
+++ coding-projects/03-ios-calculator-app/app.js
@@ -1,19 +1,63 @@
-let sayı = prompt("Sayıyı yazınız: ");
-let n1 = 0;
-let n2 = 1;
-let deger;
+let content1 = document.querySelector(".content1")
+content1.addEventListener("click", ()=>{output.innerHTML += "1"})
 
-function fibonacci(n) {
-    for (let i = 0; i <= n-1; i++) {
-        deger = n1 + n2;
-        n1 = n2;
-        n2 = deger;
-    }
-    return deger;
-}
+let content2 = document.querySelector(".content2")
+content2.addEventListener("click", ()=>{output.innerHTML += "2"})
+
+let content3 = document.querySelector(".content3")
+content3.addEventListener("click", ()=>{output.innerHTML += "3"})
+
+let content4 = document.querySelector(".content4")
+content4.addEventListener("click", ()=>{output.innerHTML += "4"})
+
+let content5 = document.querySelector(".content5")
+content5.addEventListener("click", ()=>{output.innerHTML += "5"})
+
+let content6 = document.querySelector(".content6")
+content6.addEventListener("click", ()=>{output.innerHTML += "6"})
+
+let content7 = document.querySelector(".content7")
+content7.addEventListener("click", ()=>{output.innerHTML += "7"})
+
+let content8 = document.querySelector(".content8")
+content8.addEventListener("click", ()=>{output.innerHTML += "8"})
+
+let content9 = document.querySelector(".content9")
+content9.addEventListener("click", ()=>{output.innerHTML += "9"})
+
+let content10 = document.querySelector(".content10")
+content10.addEventListener("click", ()=>{output.innerHTML += "10"})
 
 
-sayıHtml = document.querySelector("#sayı");
-sayıHtml.innerHTML = sayı;
-degerHtml = document.querySelector("#deger");
-degerHtml.innerHTML = fibonacci(sayı);
+let contentdot = document.querySelector(".contentdot")
+contentdot.addEventListener("click", ()=>{output.innerHTML += "."})
+
+
+
+let add = document.querySelector(".add ")
+add.addEventListener("click", ()=>{output.innerHTML += "+"})
+
+
+let substract = document.querySelector(".substract")
+substract.addEventListener("click", ()=>{output.innerHTML += "-"})
+
+
+let multiply = document.querySelector(".multiply")
+multiply.addEventListener("click", ()=>{output.innerHTML += "*"})
+
+
+let divide = document.querySelector(".divide")
+divide.addEventListener("click", ()=>{output.innerHTML += "/"})
+
+
+let equal = document.querySelector(".equal")
+equal.addEventListener("click", ()=>{output.innerHTML = `${number(output.innerHTML)}`})
+
+
+let clear = document.querySelector(".clear")
+clear.addEventListener("click", ()=>{output.innerHTML = ""})
+
+
+let output = document.querySelector(".output")
+
+console.log(output.innerHTML);
