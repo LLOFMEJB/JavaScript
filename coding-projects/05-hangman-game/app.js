74cb5e3af5bcb37242e7471b2522698715df35e4
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-30 23:01:42 +0300

    5

--- coding-projects/05-hangman-game/app.js
+++ coding-projects/05-hangman-game/app.js
@@ -2,34 +2,43 @@ const figures = document.querySelectorAll(".figure-part");
 const wordArea = document.querySelector("#word");
 const wrongLetters = document.querySelector("#wrong-letters");
 const warningMessage = document.getElementById("notification-container");
+const popupContainer = document.querySelector(".popup-container");
+const playBtn = document.querySelector("#play-button");
+const finalMess = document.querySelector("#final-message");
+const finalMessReveal = document.querySelector("#final-message-reveal-word");
+playBtn.addEventListener("click", () => {
+    location.reload();
+});
+popupContainer.style.display = "none";
 let word;
 // let wordLen;
 let count;
-let minPlace = "_ "
+let minPlace = "_ ";
 let placeHolder = [];
 let wrongsArr = [];
-
+let truesArr = [];
 
 async function getWord() {
-    const response = await fetch("https://random-word-api.herokuapp.com/word?number=1");
+    const response = await fetch(
+        "https://random-word-api.herokuapp.com/word?number=1"
+    );
     if (!response.ok) throw new Error("something went wrong!");
     const wordFromApi = await response.json();
     word = wordFromApi[0];
     // wordLen = word.length;
-    splittedWord = word.split("")
+    splittedWord = word.split("");
     console.log(wordFromApi);
     console.log(word);
     count = 0;
     for (let i = 0; i < word.length; i++) {
-        placeHolder.push(minPlace)
+        placeHolder.push(minPlace);
     }
     wordArea.innerHTML = placeHolder.join("");
 }
 
-window.onload = ()=>{
-    getWord()
-}
-
+window.onload = () => {
+    getWord();
+};
 
 // const figure1 = figures[0];
 // const figure2 = figures[1];
@@ -48,25 +57,64 @@ figures.forEach((item) => {
 //     console.log(word);
 // }, 1000);
 
-
-window.addEventListener("keyup", (event)=>{
+window.addEventListener("keyup", (event) => {
     if (word.includes(event.key)) {
-        for (let i = 0; i < word.length; i++) {
-            if (splittedWord[i] == event.key) {
-                placeHolder[i] = event.key.toLocaleUpperCase()+" "
-            }
-        }
-    } else {
-        if (wrongsArr.includes(event.key) ) {
-            warningMessage
-        }
-        wrongLetters.innerHTML += (event.key.toLocaleUpperCase()+" - ")
-        count++;
-        figures[count-1].style.visibility = "visible";
+        if (truesArr.includes(event.key)) {
+            warningMessage.classList.add("show");
+            setTimeout(() => {
+                warningMessage.classList.remove("show");
+            }, 1000);
+            console.log("1");
+        } 
         
+        
+        else {
+            truesArr.push(event.key);
+            for (let i = 0; i < word.length; i++) {
+                if (splittedWord[i] == event.key) {
+                    placeHolder[i] = event.key.toUpperCase() + " ";
+                }
+            }
+            console.log("2");
+        }
+        
+        
+        if (!wordArea.innerHTML.includes("_")) {
+            console.log(wrongLetters.innerHTML);
+            console.log("3");
+            finalMessReveal.innerHTML = `..the word was: ${word}`;
+            finalMess.innerHTML = "Congratulations! You did it!";
+            popupContainer.style.display = "";
+        }
     }
-
-    wordArea.innerHTML = placeHolder.join("")
-})
-
-
+    
+    
+    else {
+        if (wrongsArr.includes(event.key)) {
+            warningMessage.classList.add("show");
+            setTimeout(() => {
+                warningMessage.classList.remove("show");
+            }, 1000);
+            console.log("4");
+        } 
+        
+        
+        else {
+            wrongsArr.push(event.key);
+            wrongLetters.innerHTML += event.key.toLocaleUpperCase() + " - ";
+            count++;
+            figures[count - 1].style.visibility = "visible";
+            console.log("5");
+        }
+        
+        
+        if (wrongLetters.innerHTML.length == 24) {
+            finalMessReveal.innerHTML = `..the word was: ${word}`;
+            finalMess.innerHTML = "Ooops... You killed the poor guy!";
+            popupContainer.style.display = "";
+            console.log("6");
+        }
+    }
+    
+    wordArea.innerHTML = placeHolder.join("");
+});
